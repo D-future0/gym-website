aa33035a56d4f72ee0f14f22a674c107b95d6dc0
D-future0 <oka4collins@gmail.com>
2025-04-01 00:00:16 +0100

    navbar setup

--- src/scenes/NavBar/index.tsx
+++ src/scenes/NavBar/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid'
+// import React, { useState } from 'react'
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid'
 import Logo from '@/assets/Logo.png'
 import Link from './Link'
 import { SelectedPage } from "@/shared/SelectedPage"
